97c83966dfcda3011782692239e32a198e6b4e2b
Prashanna Raj Pandit <erprp99@gmail.com>
2025-03-27 20:52:23 -0500

    updated

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # import requests
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import pickle
 
 app=Flask(__name__)
-
 @app.route('/')
 def home():
     return render_template('index.html')
+def load_model():
+    try:
+        with open("model.pkl", "rb") as f:
+            model = pickle.load(f)
+    except FileNotFoundError:
+        raise Exception("model.pkl not found in project root")
+    return model
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST','GET'])
 def predict():
-    return render_template('index.html')
+    # if request.method=='POST':
+    #     age=int()
+    model=load_model()
+    user_input=request.form.get('temperature')
+    prediction=model.predict([[int(user_input)]])
+    prediction=round(prediction[0],2)
+    print(prediction)
+    return render_template('index.html',prediction_text=f"The total revenue generted is ${prediction}")
 
 if __name__ =="__main__":
     app.run(debug=True)
